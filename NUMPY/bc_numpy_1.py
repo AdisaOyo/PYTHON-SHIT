dcff67217321f98e11c4da369936df867d3d3d14
USER <you@example.com>
2026-09-17 09:06:45 +0100

    update

--- NUMPY/bc_numpy_1.py
+++ NUMPY/bc_numpy_1.py
@@ -1,20 +1,24 @@
 import numpy as np # this is used to give numpy a nickname for convenience, so we don't have to type numpy every time we want to use it. Instead, we can just type np.
 
-# Aggregate functions = sumarize data and typically return a single value 
+# 
 
-array = np.array([
-                    [1,2,3,4,5],
-                    [6,7,8,9,10]
-                 ])
+rng = np.random.default_rng(seed=1) # this is used to create a random number generator object. The argument 1 is the seed for the random number generator. This means that the random numbers generated will be the same every time we run the code.
 
-print(np.sum(array))
+print(rng.integers(low = 1, high = 7))
+print(rng.integers(low = 1, high = 101, size = 3))
+print(rng.integers(low = 1, high = 101, size = (3, 2) )) #will give the random numbers in a 3 row, 2 column matrix
 
-print(np.mean(array)) # mean
-print(np.std(array)) # standard deviation
-print(np.var(array)) # variance
-print(np.argmin(array)) # will return index of minimmum value 
+# if i dont set a seed Numpy will set one for me but if i do it will give me the same answer over and over again
 
-print(np.sum(array, axis=0)) # applying sum function to all columns 
+np.random.seed(seed = 1) # this is used to set the seed for the random number generator. This means that the random numbers generated will be the same every time we run the code.
+print(np.random.uniform(low = -1, high = 1, size = (3,3))) # uniform is for uniform disribution meaning every number has an equal chance of ocurring
 
+rng = np.random.default_rng()
+array = np.array([1,2,3,4,5])
+rng.shuffle(array) # this is used to shuffle the elements of the array in place. This means that the original array will be modified and the elements will be rearranged randomly.
+print(array)
 
-
+rng = np.random.default_rng()
+fruits = np.array(['apple', 'banana', 'cherry', 'date', 'elderberry'])
+fruit = rng.choice(fruits, size = 3, replace = False) # this is used to randomly select elements from the array. The argument size = 3 means that we want to select 3 elements. The argument replace = False means that we do not want to select the same element more than once.
+print(fruit)
--- NUMPY/bc_numpy_notes.py
+++ NUMPY/bc_numpy_notes.py
@@ -210,3 +210,80 @@ print(arrayi*arrayii)
 AGGREGATE FUNCTIONS
 '''
 
+import numpy as np # this is used to give numpy a nickname for convenience, so we don't have to type numpy every time we want to use it. Instead, we can just type np.
+
+# Aggregate functions = sumarize data and typically return a single value 
+
+array = np.array([
+                    [1,2,3,4,5],
+                    [6,7,8,9,10]
+                 ])
+
+print(np.sum(array))
+
+print(np.mean(array)) # mean
+print(np.std(array)) # standard deviation
+print(np.var(array)) # variance
+print(np.argmin(array)) # will return index of minimmum value 
+
+print(np.sum(array, axis=0)) # applying sum function to all columns 
+print(np.sum(array, axis=1)) # applying sum function to all rows 
+
+
+'''
+FILTERING
+'''
+
+import numpy as np # this is used to give numpy a nickname for convenience, so we don't have to type numpy every time we want to use it. Instead, we can just type np.
+
+# Filtering  = refers to the process of selecting elements from an array that match a given condition 
+
+ages = np.array([
+                  [21, 17, 19, 20, 16, 30, 18, 65],
+                  [39, 22, 15, 99, 18, 19, 20, 21]
+                ])
+
+teenagers = ages[ages < 18] # boolian indexing. this will collaps the arra into one dimension. There are ways to preserve the dimensions but we will see that later 
+print(teenagers)
+
+## adults1 = ages[18 >= ages <= 65] # this will not work because the condition is not valid. we need to use the & operator to combine the two conditions
+adults2 = ages[(ages >= 18) & (ages <65)] # this will work because the condition is valid. we need to use the & operator to combine the two conditions
+seniors = ages[ages>=65]
+print(adults2)
+print(seniors)
+
+evens = ages[ages % 2 == 0]
+print(evens)
+
+adults = np.where(ages >= 18, ages, 0)
+print(adults)
+
+
+'''
+RANDOM NUMBERS
+'''
+
+import numpy as np # this is used to give numpy a nickname for convenience, so we don't have to type numpy every time we want to use it. Instead, we can just type np.
+
+# 
+
+rng = np.random.default_rng(seed=1) # this is used to create a random number generator object. The argument 1 is the seed for the random number generator. This means that the random numbers generated will be the same every time we run the code.
+
+print(rng.integers(low = 1, high = 7))
+print(rng.integers(low = 1, high = 101, size = 3))
+print(rng.integers(low = 1, high = 101, size = (3, 2) )) #will give the random numbers in a 3 row, 2 column matrix
+
+# if i dont set a seed Numpy will set one for me but if i do it will give me the same answer over and over again
+
+np.random.seed(seed = 1) # this is used to set the seed for the random number generator. This means that the random numbers generated will be the same every time we run the code.
+print(np.random.uniform(low = -1, high = 1, size = (3,3))) # uniform is for uniform disribution meaning every number has an equal chance of ocurring
+
+rng = np.random.default_rng()
+array = np.array([1,2,3,4,5])
+rng.shuffle(array) # this is used to shuffle the elements of the array in place. This means that the original array will be modified and the elements will be rearranged randomly.
+print(array)
+
+rng = np.random.default_rng()
+fruits = np.array(['apple', 'banana', 'cherry', 'date', 'elderberry'])
+fruit = rng.choice(fruits, size = 3, replace = False) # this is used to randomly select elements from the array. The argument size = 3 means that we want to select 3 elements. The argument replace = False means that we do not want to select the same element more than once.
+print(fruit)
